86fda1a857b711a4ee61f94b44d1c0d50b1c4246
Drew Anello <drew.anello@gmail.com>
2023-09-08 04:10:09 -0400

    now generating 12 character random password

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module pw-generator
+
+go 1.21.0
--- backend/main.go
+++ backend/main.go
@@ -1,19 +1,27 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"strings"
+	"math/rand"
+	"time"
 )
-func main()  {
-	letters := make([]rune, 52)
 
+func main() {
+	length := flag.Int("length", 12, "the length of the password")
+	flag.Parse()
+	rand.Seed(time.Now().UnixNano())
+
+	letters := make([]rune, 52)
 	for i := 0; i < 26; i++ {
 		letters[i] = rune('a' + i)
 	}
 
 	for i := 0; i < 26; i++ {
-		letters[26 + i] = rune('A' + i)
+		letters[26+i] = rune('A' + i)
 	}
+
+	specialChars := "!@#$%^&*()-_=+[]{}|;:'\",.<>?/"
 
 	numbers := make([]rune, 10)
 
@@ -21,9 +29,41 @@ func main()  {
 		numbers[i] = rune('0' + i)
 	}
 
-	specialChars := "!@#$%^&*()-_=+[]{}|;:'\",.<>?/"
+	password := make([]rune, *length)
 
-	fmt.Println( strings.Split(string(specialChars), ""))
-	fmt.Println( strings.Split(string(letters), ""))
-	fmt.Println( strings.Split(string(numbers), ""))
+	password[0] = letters[rand.Intn(len(letters))]
+	password[1] = numbers[rand.Intn(len(numbers))]
+	password[2] = rune(specialChars[rand.Intn(len(specialChars))])
+
+	specialCount := 1
+
+	allChars := append(append(letters, numbers...), []rune(specialChars)...)
+
+	for i := 3; i < 12; i++ {
+		chosenChar := allChars[rand.Intn(len(allChars))]
+
+		isSpecial := false
+		for _, char := range specialChars {
+			if chosenChar == char {
+				isSpecial = true
+				break
+			}
+		}
+
+		if isSpecial && specialCount >= 2 {
+			i--
+			continue
+		}
+		password[i] = chosenChar
+
+		if isSpecial {
+			specialCount++
+		}
+	}
+
+	rand.Shuffle(len(password), func(i, j int) {
+		password[i], password[j] = password[j], password[i]
+	})
+
+	fmt.Println("Generated password:", string(password))
 }
